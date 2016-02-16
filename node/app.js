5cc2fd080fcbe1d1c56ccd051d7f477703ed544c
root <root@localhost.localdomain>
2016-02-16 16:37:07 +0100

    working

--- node/app.js
+++ node/app.js
@@ -4,3 +4,20 @@ var client = redis.createClient("6379", "redis"); //creates a new client
 client.on('connect', function() {
     console.log('connected');
 });
+
+var express = require('express');
+
+// Constants
+ var PORT = 8080;
+
+ // App
+ var app = express();
+ app.get('/', function (req, res) {
+   res.send('Hello world\n');
+   });
+
+   app.listen(PORT);
+   console.log('Running on http://localhost:' + PORT);
+
+
+
